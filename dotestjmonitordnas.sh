189a034a64ecd416faa650a4d67b11a01efeb18d
Jörg Wesemann (IT) <joerg@weseit.de>
2022-01-29 22:57:24 +0100

    docker container will now be deleted when stopped in test-script on nas

--- dotestjmonitordnas.sh
+++ dotestjmonitordnas.sh
@@ -2,5 +2,5 @@
 #mkdir /volume1/docker/shared/jmonitord
 #mkdir /volume1/docker/jpwshd/devdir/docker-jpwshd-debian/workdir
 touch ./workdir/JMonitor.csv
-docker run --name jwe_jmonitord_debian11_test --rm --restart always --env PWSHSCRIPTFILE=JMonitor.ps1 -d -v /volume1/docker/jpwshd/devdir/docker-jpwshd-debian/workdir:/root/workdir  weseit/pwshddebian11
+docker run --name jwe_jmonitord_debian11_test --rm --env PWSHSCRIPTFILE=JMonitor.ps1 -d -v /volume1/docker/jpwshd/devdir/docker-jpwshd-debian/workdir:/root/workdir  weseit/pwshddebian11
 tail -f ./workdir/JMonitor.csv
