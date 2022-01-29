8fc73212752cd61f4fd12c5c996f2e43551f3770
Jörg Wesemann <joerg@weseit.de>
2022-01-29 22:46:17 +0100

    optimzed nas testscript

--- dotestjmonitordnas.sh
+++ dotestjmonitordnas.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #mkdir /volume1/docker/shared/jmonitord
 #mkdir /volume1/docker/jpwshd/devdir/docker-jpwshd-debian/workdir
-docker run --name jwe_jmonitord_debian11_test --restart always --env PWSHSCRIPTFILE=JMonitor.ps1 -d -v /volume1/docker/jpwshd/devdir/docker-jpwshd-debian/workdir:/root/workdir  weseit/pwshddebian11
+touch ./workdir/JMonitor.csv
+docker run --name jwe_jmonitord_debian11_test --rm --restart always --env PWSHSCRIPTFILE=JMonitor.ps1 -d -v /volume1/docker/jpwshd/devdir/docker-jpwshd-debian/workdir:/root/workdir  weseit/pwshddebian11
 tail -f ./workdir/JMonitor.csv
