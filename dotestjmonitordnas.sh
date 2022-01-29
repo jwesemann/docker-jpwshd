3f4bf5b8872ea0e01b95dbb9e0e153b76c9c8562
Jörg Wesemann (IT) <joerg@weseit.de>
2022-01-29 23:14:36 +0100

    nas testscript will create workdir if neccessary ; .gitgnore - added workdir

--- dotestjmonitordnas.sh
+++ dotestjmonitordnas.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 #mkdir /volume1/docker/shared/jmonitord
-#mkdir /volume1/docker/jpwshd/devdir/docker-jpwshd-debian/workdir
+WDIR="/volume1/docker/jpwshd/devdir/docker-jpwshd-debian/workdir"
+if [ -d "$WDIR" ]; then
+	echo "$WDIR already exists"
+else
+	echo "creating directory $WDIR"
+	mkdir  "$WDIR"
+fi
+
 touch ./workdir/JMonitor.csv
 docker run --name jwe_jmonitord_debian11_test --rm --env PWSHSCRIPTFILE=JMonitor.ps1 -d -v /volume1/docker/jpwshd/devdir/docker-jpwshd-debian/workdir:/root/workdir  weseit/pwshddebian11
 tail -f ./workdir/JMonitor.csv
