cc7b88766e464f5062bf5b31d843ee610933d766
Jörg Wesemann (NAS718) <joerg@weseit.de>
2022-06-25 09:02:28 +0200

    deleted old shell scripts

--- dotestjmonitord.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#mkdir /volume1/docker/shared/jmonitord
-#mkdir /volume1/docker/shared/jmonitord/workdir
-docker run --name jwe_jmonitord_debian11 --restart always --env PWSHSCRIPTFILE=JMonitor.ps1 -d -v /mnt/c/Users/jwesemann/Documents/jwedev/3d/docker_jpwshd-debian10/workdir:/root/workdir  weseit/pwshddebian11
-tail -f ./workdir/JMonitor.csv
