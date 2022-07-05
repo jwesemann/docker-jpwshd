ff199ec31bccb3dc22bed6925479452d6af8c544
Jörg Wesemann (NAS718) <joerg@weseit.de>
2022-07-05 19:04:04 +0200

    Container start-script sets --restart=always flag

--- doprodjmonitordnas.sh
+++ doprodjmonitordnas.sh
@@ -20,5 +20,5 @@ fi
 
 
 touch ${WDIR}/JMonitor.csv
-docker run --name jwe_jmonitord_debian11_pwsh72${RUNENV} --rm --env PWSHSCRIPTFILE=JMonitorDebian11.ps1 -d -v ${WDIR}:/root/workdir  weseit/pwshddebian11
+docker run --name jwe_jmonitord_debian11_pwsh72${RUNENV} --restart=always --env PWSHSCRIPTFILE=JMonitorDebian11.ps1 -d -v ${WDIR}:/root/workdir  weseit/pwshddebian11
 tail -f ${WDIR}/JMonitorDebian11.csv
