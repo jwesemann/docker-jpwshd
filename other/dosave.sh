582dabd59cf140d6d261fc3bfd779549dd7ca9e7
Jörg (kali-tuxedo) Wesemann <joerg@weseit.de>
2025-08-20 16:49:06 +0200

    Delete outdated scripts

--- other/dosave.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-docker save weseit/pwshddebian11 | gzip >weseit_pwshddebian11.tar.gz
-
--- other/dosetgitremote.ps1
+++
@@ -1,3 +0,0 @@
-git remote -v
-git remote set-url origin git@github.com:jwesemann/docker-jpwshd-debian.git
-git remote -v
